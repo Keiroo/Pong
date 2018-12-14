4f2d56a1b6770d35a3822802ff7749f307c8f881
Keiroo <orynia55@o2.pl>
2018-12-14 22:29:35 +0100

    Working on collisions

--- Source/Pong/Ball.cpp
+++ Source/Pong/Ball.cpp
@@ -8,12 +8,30 @@ ABall::ABall()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+	//SphereCollider = CreateDefaultSubobject<USphereComponent>("SphereCollider");
+
+	StaticMesh->BodyInstance.SetCollisionProfileName("OverlapAll");
+	StaticMesh->OnComponentBeginOverlap.AddDynamic(this, &ABall::OnOverlapBegin);
+	StaticMesh->OnComponentEndOverlap.AddDynamic(this, &ABall::OnOverlapEnd);
+
+
+	/*StaticMesh->SetSimulatePhysics(true);
+	StaticMesh->SetNotifyRigidBodyCollision(true);
+	StaticMesh->BodyInstance.SetCollisionProfileName("Pawn");
+	StaticMesh->OnComponentHit.AddDynamic(this, &ABall::OnComponentHit);*/
+
+	/*SphereCollider->SetSimulatePhysics(true);
+	SphereCollider->SetNotifyRigidBodyCollision(true);
+	SphereCollider->BodyInstance.SetCollisionProfileName("Pawn");
+	SphereCollider->OnComponentHit.AddDynamic(this, &ABall::OnComponentHit);*/
 }
 
 // Called when the game starts or when spawned
 void ABall::BeginPlay()
 {
 	Super::BeginPlay();
+
 	
 }
 
@@ -22,6 +40,14 @@ void ABall::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (isMoving)
+	{
+		FVector NewLocation = GetActorLocation();
+		NewLocation.X += BallSpeed * DeltaTime;
+		NewLocation.Y += BallSpeed * DeltaTime;
+
+		SetActorLocation(NewLocation);
+	}
 }
 
 // Called to bind functionality to input
@@ -31,3 +57,15 @@ void ABall::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 }
 
+void ABall::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL))
+	{
+		isMoving = false;
+		if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(*OtherActor->GetName()));
+	}
+}
+
+void ABall::OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+}
--- Source/Pong/Ball.h
+++ Source/Pong/Ball.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Components/SphereComponent.h"
 #include "Ball.generated.h"
 
 UCLASS()
@@ -18,6 +19,7 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	bool isMoving = true;
 
 public:	
 	// Called every frame
@@ -26,4 +28,19 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(EditAnywhere)
+	float BallSpeed = 100.0f;
+
+	/*UPROPERTY(EditAnywhere)
+	USphereComponent* SphereCollider;*/
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	/** called when something leaves the sphere component */
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
